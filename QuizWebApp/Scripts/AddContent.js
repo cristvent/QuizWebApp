aebb7770ead6f2e0a2a5a62508da855849e7c3a2
Cristian Ventura <cristian101ventura@yahoo.com>
2017-03-08 09:19:44 -0500

    Finished with the adding and editing features. Moving on to the delete ability and changing styling.

--- QuizWebApp/Scripts/AddContent.js
+++ QuizWebApp/Scripts/AddContent.js
@@ -1,35 +1,39 @@
 ﻿$(document).ready(function () {
     var addContentButton = document.getElementById("add-content-button"),
         contentHolder = document.getElementById("question-holder"),
-        addAnswerButton,
-        nextId = 0;
+        addAnswerButton;
+
+    var questionCounter = document.getElementById("question-count");
+    var nextId = parseInt(questionCounter.innerText);
 
     var questionTemplate =
-    '<div id="question-heading" class="panel-heading">' +
-    '<h5> Question {{~}}</h5>' +
+    '<div class="panel-heading question-heading">' +
+    '<h5><strong> Question {{#}} </strong></h5>' +
     '</div>' +
     '<div id="question-body" class="panel-body">' +
-    '<input type="text" id="Questions[{{~}}].Content" name="Questions[{{~}}].Content" class="form-control"/>' +
-    '<a href="#{{~}}" data-toggle="collapse">Q {{~}} - Answers</a>' +
+    '<input type="text" id="Questions[{{~}}].Id" name="Questions[{{~}}].Id" value="{{~}}"class="hidden"/>' +
+    '<input type="text" id="Questions[{{~}}].Content" name="Questions[{{~}}].Content" class="form-control question-content-input"/>' +
+    '<a href="#{{~}}" data-toggle="collapse">Answers</a>' +
     '<div id="{{~}}" class="collapse answers-body">' +
-    '<button type="button" class="btn btn-success btn-xs" id="add-answer-button-{{~}}"> Add Answer </button>' +
+    '<button type="button" class="btn btn-success btn-xs add-button" id="add-answer-button-{{~}}"> Add Answer </button>' +
     '</div>' +
     '</div>' +
     '</div>';
 
     var answerTemplate =
-    '<div id="new-answer-holder" class="col-md-12">' +
-    '<div class="col-md-8">' +
-    '<input type="text" id="{{~}}" name="{{~}}" class="form-control"/>' +
+    '<div class="col-md-12 new-answer-holder">' +
+    '<div class="col-md-8 answer-content-input">' +
+    '<input type="text" id="{{~}}.Id" name="{{~}}.Id" class="hidden"/>' +
+    '<input type="text" id="{{~}}.Content" name="{{~}}.Content" class="form-control"/>' +
     '</div>' +
-    '<div class="col-md-4">' +
-    '<input type="checkbox" id="{{#}}" name="{{#}}" class="form-control edit-checkbox" value="true" />' +
-    '<input type="hidden" id="{{#}}" name="{{#}}" class="form-control" value="false" />' +
+    '<div class="col-md-4 answer-iscorrect-input">' +
+    '<input type="checkbox" id="{{#}}.IsCorrect" name="{{#}}.IsCorrect" class="form-control edit-checkbox" value="true" />' +
+    '<input type="hidden" id="{{#}}.IsCorrect" name="{{#}}.IsCorrect" class="form-control" value="false" />' +
     '</div>' +
     '</div>';
 
     addContentButton.addEventListener("click", function () {
-        var questionTemp = questionTemplate.replace(/{{~}}/g, nextId);
+        var questionTemp = questionTemplate.replace(/{{~}}/g, nextId).replace(/{{#}}/g, (nextId + 1));
         var questionHolder = document.createElement("div");
         questionHolder.classList.add("panel");
         questionHolder.classList.add("panel-default");
@@ -43,8 +47,8 @@
         var answerHolder = document.getElementById(this.parentElement.id);
         var answerCounter = (answerHolder.childElementCount) - 1;
         var answerTemp = document.createElement("div");
-        var tempWithReplaces = answerTemplate.replace(/{{~}}/g, "Questions[" + answerHolder.id + "].Answers[" + answerCounter + "].Content")
-            .replace(/{{#}}/g, "Questions[" + answerHolder.id + "].Answers[" + answerCounter + "].IsCorrect");
+        var tempWithReplaces = answerTemplate.replace(/{{~}}/g, "Questions[" + answerHolder.id + "].Answers[" + answerCounter + "]")
+            .replace(/{{#}}/g, "Questions[" + answerHolder.id + "].Answers[" + answerCounter + "]");
         answerTemp.innerHTML = tempWithReplaces;
         answerHolder.appendChild(answerTemp);
     }
