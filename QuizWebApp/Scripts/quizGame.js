398097969a0a066bc07dfceed5ccb42c13e1e9c4
Cristian Ventura <cristian101ventura@yahoo.com>
2017-03-21 10:30:43 -0400

    Finished quiz game concept. All questions display and answers get correctly checked. First iteration on the game function complete.

--- QuizWebApp/Scripts/quizGame.js
+++ QuizWebApp/Scripts/quizGame.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
+    // Delcare needed variables
     var nextQuestionButton = document.getElementById('next-question-button'),
+        lastQuestionButton = document.getElementById('last-question-button'),
         questionArea = document.getElementById('question-area'),
         quizId = document.getElementById("quizid-holder"),
         currentQuestionId,
@@ -9,13 +11,35 @@
         questContent = document.getElementById("quest-content"),
         questAnswers = document.getElementById("quest-answers");
 
+    // Call function to get quiz from DB based on passed ID
     getSessionQuiz();
 
+    // Event handler for next question button click
     nextQuestionButton.addEventListener("click", function () {
         clearContent();
+        questionCounter++;
+        if (questionCounter >= sessionQuiz.Questions.length) {
+            clearContent();
+            questContent.innerText = "All Questions Have Been Asked";
+            questNum.innerText = "Done";
+            nextQuestionButton.innerText = "Start Over";
+            questionCounter = -1;
+            return;
+        }
         displayQuestion();
     });
 
+    // Event handler for last question button click
+    lastQuestionButton.addEventListener("click", function () {
+        clearContent();
+        questionCounter--;
+        if (questionCounter < 0) {
+            questionCounter = 0;
+        }
+        displayQuestion();
+    })
+
+    // Using AJAX to make a REST call. Getting the quiz, questions, and answers from DB.
     function getSessionQuiz() {
         var callUrl = '/api/quizgame?id=' + quizId.innerText;
         $.getJSON(callUrl, function (data) {
@@ -24,11 +48,10 @@
         });
     };
 
+    // Function to get content and holders ready to display
     var displayQuestion = function displayQuestion() {
-        if (questionCounter >= sessionQuiz.Questions.length) {
-            clearContent();
-            questContent.innerText = "All Questions Have Been Asked";
-            return;
+        if (nextQuestionButton.innerText === "Start Over") {
+            nextQuestionButton.innerText = "Next";
         }
         currentQuest = sessionQuiz.Questions[questionCounter];
         questContent.innerText = currentQuest.Content;
@@ -44,17 +67,21 @@
             item.addEventListener("click", isAnswerCorrect, false);
             questAnswers.appendChild(item);
         }
-        questionCounter++;
     }
 
+    // Using AJAX to make a REST call. Checking the DB if clicked answer is correct
     function isAnswerCorrect() {
         var callUrl = "/api/quizgame/isanswercorrect?answerId=" + this.id;
         element = document.getElementById(this.id);
         $.getJSON(callUrl, function (data) {
             element.style.backgroundColor = (data.IsCorrect === true) ? "#def0de" : "#f7dcdb";
+            if (data.IsCorrect === true) {
+
+            }
         });
     }
 
+    // Clear holders 
     var clearContent = function clearContent() {
         questNum.innerText = "";
         questContent.innerText = "";
